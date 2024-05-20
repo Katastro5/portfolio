471a8be6ceae3c7274516be344dea733a6c71875
Angelos Tatsis <168748109+Katastro5-Official@users.noreply.github.com>
2024-05-20 16:54:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function updateVisits() {
   try {
-    const response = await fetch(process.env.API_URL);
+    const response = await fetch("https://games.roblox.com/v1/games?universeIds=1842715780");
     const data = await response.json();
     const visits = data.data[0].visits;
     const visitsElement = document.querySelector('.project-1-visits');
